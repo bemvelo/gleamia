b4a8a8fbde844cf28538e3e1589f1fbae70520d1
bemvelo <buhlebemvelotshabangu@gmail.com>
2026-03-02 13:18:22 +0200

    Edited cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import NavBar from "@/../../components/NavBar";
+import NavBar from "../../components/NavBar";
 
 type Product = {
   id: string;
