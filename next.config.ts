50d3874cb39dc2d3bcfc85f8b3bcd1131899d2a7
bemvelo <buhlebemvelotshabangu@gmail.com>
2026-03-19 10:06:18 +0200

    fix: simplify next.config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbo: {
-    resolveAlias: {
-      "@/lib": "./lib",
-      "@/components": "./components",
-      "@/app": "./app",
-      "@/src": "./src",
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
